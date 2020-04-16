c77bb7a10b36bcd893d5e79e15a18525c67d3d42
Nik Bez <nikitabit@yandex.ru>
2020-04-16 17:12:53 +0200

    Переделал вообще все. Был пустой файл, теперь тут буду разрабатывать игру на базе tkinter

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-import tkinter as tk
-root = tk.Tk()
-e = Entry(root, width = 20)
-b = Button(root, text = 'Преобразовать')
-l = Lable(root, bg='black', fg = 'white', width =20)
+from tkinter import *
+from random import randrange as rnd, choice
+import time
+root = Tk()
+root.geometry('800x600')
+
+canv = Canvas(root,bg='white')
+canv.pack(fill=BOTH,expand=1)
+
+colors = ['red','orange','yellow','green','blue']
+def new_ball():
+    global x, y, r
+    canv.delete(ALL)
+    x = rnd(100,700)
+    y = rnd(100,500)
+    r = rnd(30,50)
+    canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0)
+    root.after(1000,new_ball)
+
+
+def click(event):
+    print(x,y,r)
+
+new_ball()
+canv.bind('<Button-1>', click)
+mainloop()
